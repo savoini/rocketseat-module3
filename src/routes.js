8a33f3fac234c06bd6f3acf4cf2630adf06428ce
Gilberto <savoini@dac246.dac.unicamp.br>
2019-04-29 16:04:34 -0300

    Criando rota de usuário

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const UserController = require('./app/controllers/UserController')
 
 const routes = express.Router()
 
 routes.get('/', (req, res) => res.json({ msg: 'Hello World' }))
+routes.post('/users', UserController.store)
 
 module.exports = routes
--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const databaseConfig = require('./config/database')
 
 class App {
   constructor () {
     this.express = express()
     this.isDev = process.env.NODE_ENV !== 'production'
 
+    this.database()
     this.middleware()
     this.routes()
+  }
+
+  database () {
+    mongoose.connect(databaseConfig.uri, {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    })
   }
 
   middleware () {
