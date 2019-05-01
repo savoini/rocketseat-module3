42da611646449ba89c68e217f422b80a79727d19
Gilberto Savoini <gilberto.savoini@gmail.com>
2019-05-01 19:39:55 -0300

    Template de email

--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -13,7 +13,8 @@ class PurchaseController {
       from: '"Gilberto Savoini" <savoini@unicamp.br>',
       to: purchaseAd.author.email,
       subject: `Solicitação de compra: ${purchaseAd.title}`,
-      html: `<p>${content}</p>`
+      template: 'purchase',
+      context: { user, content, ad: purchaseAd }
     })
 
     return res.send()
