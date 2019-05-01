51fb90a66eb8f6fcdab8b0671213a5fcc85b729d
Gilberto Savoini <gilberto.savoini@gmail.com>
2019-05-01 20:21:35 -0300

    Erros

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,22 @@
 const express = require('express')
-const authMiddleware = require('./app/middleware/auth')
+const validate = require('express-validation')
 
+const authMiddleware = require('./app/middleware/auth')
 const controllers = require('./app/controllers')
+const validators = require('./app/validators')
 
 const routes = express.Router()
 
-routes.post('/users', controllers.UserController.store)
-routes.post('/sessions', controllers.SessionController.store)
+routes.post(
+  '/users',
+  validate(validators.User),
+  controllers.UserController.store
+)
+routes.post(
+  '/sessions',
+  validate(validators.Session),
+  controllers.SessionController.store
+)
 
 routes.use(authMiddleware)
 
@@ -19,13 +29,17 @@ routes.use(authMiddleware)
  */
 routes.get('/ads', controllers.AdController.index)
 routes.get('/ads/:id', controllers.AdController.show)
-routes.post('/ads', controllers.AdController.store)
-routes.put('/ads/:id', controllers.AdController.update)
+routes.post('/ads', validate(validators.Ad), controllers.AdController.store)
+routes.put('/ads/:id', validate(validators.Ad), controllers.AdController.update)
 routes.delete('/ads/:id', controllers.AdController.destroy)
 
 /**
  * Purchase
  */
-routes.post('/purchase', controllers.PurchaseController.store)
+routes.post(
+  '/purchase',
+  validate(validators.Purchase),
+  controllers.PurchaseController.store
+)
 
 module.exports = routes
