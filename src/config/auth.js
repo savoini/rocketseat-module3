499c73abdb277dd1e8ad9aa1b18409c2ab1fc17a
Gilberto Savoini <gilberto.savoini@gmail.com>
2019-05-01 22:53:06 -0300

    Variavel de ambiente

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 module.exports = {
-  secret: 'goNode03',
+  secret: process.env.APP_SECRET,
   ttl: 86400
 }
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,9 @@
 module.exports = {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
-  secure: false,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: process.env.MAIL_SECURE,
   auth: {
-    user: 'd4bf24198e04cf',
-    pass: '82140995755026'
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   }
 }
